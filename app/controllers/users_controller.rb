bd91641f685f56b2cbe434cf26cf5dc34669de78
Pwako11 <akolo@hotmail.com>
2021-05-13 16:13:02 -0400

    updated user controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,36 @@
 class UsersController < ApplicationController
 
+    def show
+
+    end 
+    
+    def show
+        user = set_user
+        if user
+            render json: user
+        else
+            render json: {error: "Please enter a valid user name and email"}
+        end 
+    end 
+
+    def create 
+        user = User.new(user_params)
+
+        if user.save
+            render json: {user: user, message: "Created Successfully"}
+        end 
+    end 
+
+    private
+
+    def set_user
+        user = User.find(params[:id])
+    end 
+
+    def user_params
+        params.require(:user).permit(:name, :email)
+    end 
+     
+
     
 end
