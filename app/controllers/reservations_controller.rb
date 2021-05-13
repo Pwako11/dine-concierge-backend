d55acc08682f3731a63214b641f604a23513bd11
Pwako11 <akolo@hotmail.com>
2021-05-13 15:59:05 -0400

    reservation controller updates

--- app/controllers/reservations_controller.rb
+++ app/controllers/reservations_controller.rb
@@ -1,2 +1,38 @@
 class ReservationsController < ApplicationController
+    before_action :set_reservation, only: [:destroy]
+    
+    def index
+        render json: Reservation.all
+    end 
+
+    def show
+        reservation = set_reservation
+        if reservation
+            render json: reservation
+        else
+            render json: {error: "No reservation found"}
+        end 
+    end 
+
+    def create 
+        reservation = Reservation.new(reservation_params)
+
+        if reservation.save
+            render json: {reservation: reservation, message: "Created Successfully"}
+        end 
+    end 
+
+    def destroy
+        reservation.destroy
+    end 
+
+    private
+
+    def set_reservation
+        reservation = Reservation.find(params[:id])
+    end 
+
+    def reservation_params
+        params.require(:reservation).permit(:restaurant_name, :booked_time, :booked_email, :notes, :reserved, :user_id)
+    end 
 end
